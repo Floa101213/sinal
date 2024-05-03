88434a431531983ae08b96b7012f98b9e3431ed9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 20:37:15 -0300

    concluida

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int numero;
+
+Console.Write(" Digite um número: ");
+
+numero = Convert.ToInt32(Console.ReadLine());
+if (numero < 0)
+{
+    Console.WriteLine("negativo");
+}
+else if (numero > 0)
+{
+    Console.WriteLine("positivo");
+}
+else
+{
+    Console.WriteLine("zero");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
